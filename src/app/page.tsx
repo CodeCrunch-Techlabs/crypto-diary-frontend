6b20ba9c8f2ceca3fe9035140db7ccf624eca61a
dhruv <dhruv@dhruvs-MacBook-Air.local>
2025-01-21 16:21:57 +0530

    testing ci-cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Welcome to the CryptoDiary Devs</h1>
+      <h1 className="text-4xl font-bold text-blue-500">Welcome to CryptoDiary</h1>
     </main>
   );
 }
