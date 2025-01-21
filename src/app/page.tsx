145484ec6677daea4b4c3c496ea93bc3f8393541
dhruv <dhruv@dhruvs-MacBook-Air.local>
2025-01-21 15:17:00 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Welcome to CryptoDiary</h1>
+      <h1 className="text-4xl font-bold text-blue-500">Welcome Devs to CryptoDiary</h1>
     </main>
   );
 }
