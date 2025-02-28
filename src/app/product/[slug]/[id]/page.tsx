f65bdcbc56616f0d814271168dec2ea72bdf85c8
dhruv <dhruv@dhruvs-MacBook-Air.local>
2025-02-28 14:03:26 +0530

    make png to favicon

--- src/app/product/[slug]/[id]/page.tsx
+++ src/app/product/[slug]/[id]/page.tsx
@@ -39,7 +39,7 @@ export async function generateMetadata({ params }: { params : Promise<{ id: stri
       url: `${BASE_URL}/product/${slug}/${id}`,
       images: [
         {
-          url: product?.logo_url ? product.logo_url : "/favicon.ico",
+          url: product?.logo_url ? product.logo_url : "/favicon.png",
           width: 1200,
           height: 630,
           alt: product?.name || "CryptoDiary",
@@ -79,7 +79,7 @@ export default async function ProductDetail({ params }: { params: Promise<{ id: 
       "@type": "Organization",
       name: "CryptoDiary",
       url: BASE_URL, 
-      logo: `${BASE_URL}/favicon.ico` 
+      logo: `${BASE_URL}/favicon.png` 
     },
     offers: {
       "@type": "Offer",
