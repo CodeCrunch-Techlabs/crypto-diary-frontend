3f7fb5d5f9967c16ed45d1479b27af5355197c40
dhruv <dhruv@dhruvs-MacBook-Air.local>
2025-03-06 00:38:10 +0530

    changed button name

--- src/components/Product/ProductDetail.tsx
+++ src/components/Product/ProductDetail.tsx
@@ -100,7 +100,7 @@ const ProductDetailPage: React.FC<ProductDetailProps> = ({ product, jsonLd }) =>
               rel="noopener noreferrer"
               className="inline-block px-6 py-3 text-sm font-semibold text-black bg-green-500 dark:bg-green-400 hover:bg-green-600 dark:hover:bg-green-500 rounded-md shadow transition-colors color:black"
             >
-              Visit Product Website
+              Website
             </a>
           </div>
         </div>
